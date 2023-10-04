56a628f6732aeb5ac650db9aae53737531c3f38c
shibam-naskar <shibamnskar@gmail.com>
2023-10-04 11:53:32 +0530

    added cors origin

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ dotenv.config();
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://jolubook.vercel.app",
     // credentials: true,
   },
 });
